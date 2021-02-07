afbb488efb3bbd3c7c695ed5b8afa803fedbfda9
Md Nayem Hossain <2191081070@uttarauniversity.edu.bd>
2021-02-07 20:25:41 +0600

    complete again

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 document.getElementById('search-btn').addEventListener('click', function() {
     const inputText = document.getElementById('input-text').value;
 
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputText}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`)
         .then(res => res.json())
         .then(data => {
             const allFood = document.getElementById('all-food-div');
             const mealItem = data.meals;
             let addHtml ='';
-            if (mealItem) {
+            if (inputText === '') {
+                alert('wrong')
+            }
+            else{
                 mealItem.forEach(foodName => {
                     addHtml +=`
                         <div onclick="allFoodDiv(${foodName.idMeal})" class = 'food-item'>
@@ -17,20 +20,15 @@ document.getElementById('search-btn').addEventListener('click', function() {
                     `
                 });
             }
-            else{
-                alert('Your food is not available');
-            }
             allFood.innerHTML = addHtml;
         })
 })
 
 const allFoodDiv = mealID => {
-    document.getElementById('all-food-div').style.display = 'none';
     const eachFood = document.getElementById('each-food');
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
         .then(res => res.json())
         .then(data =>{
-            console.log(data);
             const addHtml = `
                 <div class="single-food">
                     <img src="${data.meals[0].strMealThumb}">
